ef5553ae1d9d150fc62f7f32ec51c1c5ba3d6a1b
Colton <Alekhen@gmail.com>
2014-10-18 17:34:45 -0700

    Added 'Email' Class Methods

--- classes/email.php
+++ classes/email.php
@@ -12,6 +12,46 @@
 
 class Email {
 
+	public $args = [
+		'sender' => '',			// Email address of sender (default to admin email)
+		'reply_to' => '',		// Email address recipients may reply to (default to admin email)
+		'recipient' => '',		// Email address to which the email will be sent
+		'subject' => '',		// Subject line of the email
+		'message' => '',		// HTML email string to be sent to recipient
+		'template' => '',		// File name of the HTML email template (templates must be located in lib/views/email)
+		'data' => array()		// Array of data to be used by the HTML email template
+	];
 
+	public function __construct( $args ) {
+
+		if( isset( $args ) && is_array( $args ) ) {
+			foreach( $args as $arg => $value ) {
+				$this->args[$arg] = $value;
+			}
+		}
+
+		$this->send_mail();
+
+	}
+
+	protected function send_mail() {
+
+		extract( $this->args );
+
+		$r = "\r\n";
+		$headers = "From: " . $sender . $r;
+		$headers .= !empty( $reply_to ) ? "Reply-To: " . $reply_to . $r : "";
+		$headers .= "Mime-Version: 1.0" . $r;
+		$headers .= "Content-type: text/html; charset=UTF-8" . $r;
+		$headers .= "X-Mailer: PHP/" . phpversion();
+
+		ob_start();
+		require_once EMAIL_DIR . $template . '.php';
+		$message = ob_get_contents();
+		ob_end_clean();
+
+		@mail( $recipient, $subject, $message, $headers );
+
+	}
 
 }
