fd16e4144ee7186df79afe517c657e16cf34033f
Colton <Alekhen@gmail.com>
2014-10-21 21:51:11 -0700

    Built Initial Admin Dashboard HTML Structure

---
+++ admin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Admin Dashboard
+ *
+ * @package    Subscribe LP
+ * @author     Colton James Wiscombe <colton@hazardmediagroup.com>
+ * @copyright  2014 Hazard Media Group LLC
+ * @license    MIT License - http://www.opensource.org/licenses/mit-license.html
+ * @link       https://github.com/Alekhen/subscribe-lp
+ * @version    Release: 0.1 (ALPHA)
+ */
+
+require_once dirname( __FILE__ ) . '/config/config.php';
+
+?><!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<title><?= 'Admin Dashboard | ' . SITE_NAME ?></title>
+
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<link rel="shortcut icon" href="<?= ROOT_DIR . 'favicon.ico' ?>">
+
+</head>
+<body>
+
+	<h1>ADMIN</h1>
+
+</body>
+</html>
